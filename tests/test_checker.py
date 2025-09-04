2a0c03a19e24d75e8e638f858e30c2a0dbe50373
Jimena Beltran Lazo <jbeltranlazo3@gmail.com>
2025-09-04 15:05:10 -0300

    Implementación Checker con Tests

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -7,23 +7,23 @@ class TestChecker(unittest.TestCase):
         self.checker = Checker(color="Negro", posicion=1)
 
     def test_inicio_ficha(self):
-        self.assertEqual(self.checker.color, "Negro")
-        self.assertEqual(self.checker.posicion, 1)
-        self.assertTrue(self.checker.en_juego)
+        self.assertEqual(self.checker.__color__, "Negro")
+        self.assertEqual(self.checker.__posicion__, 1)
+        self.assertTrue(self.checker.__en_juego__)
 
     def test_mover_ficha_valido(self):
         self.checker.mover_a(5)
-        self.assertEqual(self.checker.posicion, 5)
+        self.assertEqual(self.checker.__posicion__, 5)
 
     def test_mover_ficha_invalido(self):
         # Se intenta mover a posición fuera de rango (24 casillas en total)
         with self.assertRaises(ValueError):
             self.checker.mover_a(25)
         # Posición original no cambia
-        self.assertEqual(self.checker.posicion, 1)
+        self.assertEqual(self.checker.__posicion__, 1)
 
     def test_desactivar_ficha(self):
-        self.checker.en_juego = False # quedó capturada
-        self.checker.posicion = None  # ya salió del tablero
-        self.assertFalse(self.checker.en_juego)
-        self.assertIsNone(self.checker.posicion)
+        self.checker.__en_juego__ = False # quedó capturada
+        self.checker.__posicion__ = None  # ya salió del tablero
+        self.assertFalse(self.checker.__en_juego__)
+        self.assertIsNone(self.checker.__posicion__)
